3dc8e436fb486469658ce81f965eb958b6876fee
eunjin466 <dmswls10917@naver.com>
2025-01-17 12:58:01 +0900

    0116

--- java_study1/src/study0117/JavaType.java
+++ java_study1/src/study0117/JavaType.java
@@ -1,5 +1,16 @@
 package study0117;
 
 public class JavaType {
+	
+		public static void main(String[] args) {
+			
+			int num =20;
+			float pi=3.14f;
+			
+			pi=num;		//자동 형변환
+			num=(int)pi; 	//강제 형변환- 일시적인 타입변환임
+			char ch = (char)pi;
+			System.out.println()
+		}
 
 }
