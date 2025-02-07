e00790309617397b9bb7191537ef3d20b33252ff
eunjin466 <dmswls10917@naver.com>
2025-02-07 12:53:25 +0900

    0207

--- java_study1/src/study0206/Javaloop4.java
+++ java_study1/src/study0206/Javaloop4.java
@@ -6,48 +6,82 @@ public class Javaloop4 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
-		int sum = 0;
-		while(true) {
-			System.out.println("정수입력: ");
-			int num= sc.nextInt();
+		int me =0, com = 0, num =1;
+		while(true) {//언제 31이 나올지 모르니 무한루프로 전체를 반복
+		System.out.print("숫자 몇개: ");
+		me = sc.nextInt();
+		// 1~3 사이 값입력했는지 체크 하는 반복문 만들면 조지
+		System.out.print("나: ");
+		for(int i=1; i<=me; i++) {
+			System.out.println((num++) + "   ");
+			if(num == 32) break;// for문 종료
 			
-			if(num ==0) {
-				System.out.println("총 숫자합: "+sum);
-				System.out.println("끝"); break;
-				
-			}else {
-				sum = sum+num;
-				
-			}
 		}
-		
-		int lose = 0; 
-		int same = 0;
-		final int 내기 = 10;
-		
-		while(true) {
-			System.out.print("1 가위,2 바위, 3 보");
-			int user= sc.nextInt();
-			int com = (int)(Math.random()* 3+1);
-			
-			if(user == 1 && com == 2 || user ==2 && com == 3 || user == 3 && com == 1) {
-				System.out.println("졌당ㅠㅠ");
-				lose++;
-				
-			}else if (user == 1 && com == 3 || user == 2 && com == 1 || user == 3 && com == 2 ) {
-				System.out.println("이겼당!!"); 
-			}else {
-				System.out.println("비겼당");
-				same++;
-				user = user - 내기;
-				if(user < 내기); break;
-			}
-			
-			System.out.println("진 횟수: " + lose );
-			System.out.println("비긴 회수: " + same);
+		if(num == 32) {
+			System.out.println("내가 졌다..");
+			break; // while 종료
 			
 		}
 		
+		com = (int)(Math.random() * 3+1); // 1~3개 중하나
+		System.out.print("\n컴퓨터: ");
+		for(int i=1; i<=com; i++) {
+			System.out.println((num++) + "   ");
+		}
+		if(num ==32) {
+			System.out.println("컴퓨터 패배!!");
+			break;
+		}
+		}
+		
+		
+		
+		
+		
+		
+//		
+//		int sum = 0;
+//		while(true) {
+//			System.out.println("정수입력: ");
+//			int num= sc.nextInt();
+//			
+//			if(num ==0) {
+//				System.out.println("총 숫자합: "+sum);
+//				System.out.println("끝"); break;
+//				
+//			}else {
+//				sum = sum+num;
+//				
+//			}
+//		}
+//		
+//		int lose = 0; 
+//		int same = 0;
+//		final int 내기 = 10;
+//		
+//		while(true) {
+//			System.out.print("1 가위,2 바위, 3 보");
+//			int user= sc.nextInt();
+//			int com = (int)(Math.random()* 3+1);
+//			
+//			if(user == 1 && com == 2 || user ==2 && com == 3 || user == 3 && com == 1) {
+//				System.out.println("졌당ㅠㅠ");
+//				lose++;
+//				
+//			}else if (user == 1 && com == 3 || user == 2 && com == 1 || user == 3 && com == 2 ) {
+//				System.out.println("이겼당!!"); 
+//			}else {
+//				System.out.println("비겼당");
+//				same++;
+//				user = user - 내기;
+//				if(user < 내기); break;
+//			}
+//			
+//			System.out.println("진 횟수: " + lose );
+//			System.out.println("비긴 회수: " + same);
+//			
+//		}
+//		
 		
 		
 		
