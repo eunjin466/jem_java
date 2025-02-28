231ed8885f670b43551e1d0088064b16dc7c733f
eunjin466 <dmswls10917@naver.com>
2025-02-28 12:01:06 +0900

    0228

---
+++ Java_interface/src/interface1/mainMt.java
@@ -0,0 +1,14 @@
+package interface1;
+
+public class mainMt {
+	public static void main(String[] args) {
+		
+		store 분식 = new 떡볶이;
+		store 감성 = new Cafe;
+		
+		
+		분식.cooking();
+		감성.cooking();
+		
+	}
+}
---
+++ Java_interface/src/interface1/떡볶이.java
@@ -0,0 +1,11 @@
+package interface1;
+
+public class 떡볶이 implements store{
+
+	@Override
+	public void cooking() {
+		System.out.println("떡볶이를 만들었다.");
+		
+	}
+
+}
