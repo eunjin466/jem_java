6cf8b29220939f61d538564ab930445b278fc971
eunjin466 <dmswls10917@naver.com>
2025-03-05 10:07:47 +0900

    0304

---
+++ Java_generic/src/generic1/Drink.java
@@ -0,0 +1,11 @@
+package generic1;
+
+public class Drink<a> {
+	a name;
+	int ml;
+
+	public void 경림추천 (String name) {
+		this.name = name;
+	}
+	
+}
---
+++ Java_generic/src/generic1/MainMt.java
@@ -0,0 +1,36 @@
+package generic1;
+
+public class MainMt {
+
+	public static void main(String[] args) {
+			
+		Drink<String> d = new Drink<String>();
+		d.name = "토마토주스";
+		d.ml = 350;
+		d.경림추천 ="토마토주스"
+	
+		
+		
+		
+		
+		
+/*
+		** 제네릭 - 속에 특유한 , 속명  	상표명 - 일반명
+		- 클래스 내부에서 사용할 데이터 타입을 외부에서 지정하는 방법
+		
+		class A {
+			int num;
+			float fnum;
+			String name;
+		}
+		
+		class B {
+			int num;
+			float fnum;
+			String name;
+		}
+*/		
+		
+	}
+
+}
